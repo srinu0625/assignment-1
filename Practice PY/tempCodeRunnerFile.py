dbe59c219f44eb4db86a9b16c055bc8817f7c58e
srinu0625 <srinivasreddy.d0625@gmail.com>
2025-04-15 18:04:14 +0530

    15-04-25 6:05:00 pm

--- Practice PY/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-                num_of_lots = math.floor(risk / max_loss_for_trade )
-                if num_of_lots >=max_num_lots:
-                   num_of_lots = 5
-            entry_price = local_high1 + (tick_val * 2)
